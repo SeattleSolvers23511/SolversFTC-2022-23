f5851e10382e0d8d1fe3538ab4c4614d94288984
Arush Yadlapati <arushyadlapati.lwsd@gmail.com>
2023-08-14 15:39:27 -0700

    - Added what mode the robot is on for Combined_MecanumTeleOp.java using telemetry.addData();

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Combined_MecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Combined_MecanumTeleOp.java
@@ -71,99 +71,97 @@ public class Combined_MecanumTeleOp extends LinearOpMode {
                 telemetry.update();
             }
 
+            // Get raw values from the gamepad
+            double y = -gamepad1.left_stick_y; // Negative because the gamepad's y-axis is inverted
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
+
+            double forward, sideways, rotation;
+
             if (isFieldCentric) {
-                runFieldCentricMode();
+                // Convert the raw x and y values to field-centric forward and sideways velocities
+                forward = y * Math.cos(getBotHeading()) + x * Math.sin(getBotHeading());
+                sideways = -y * Math.sin(getBotHeading()) + x * Math.cos(getBotHeading());
+                rotation = rx;
+
+                // Use the LT value as an acceleration factor.
+                // LT value is between 0 (not pressed) and 1 (fully pressed).
+                double lt = gamepad1.left_trigger;
+                double speed = accelerationFactor + (1 - accelerationFactor) * lt;
+
+                // Reset the yaw angle to 0 degrees when the "Back" button is pressed.
+                if (gamepad1.back) {
+                    imu.resetYaw();
+                }
+
+                // Calculate motor powers using mecanum drive kinematics
+                double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+                // Rotate the movement direction counter to the bot's rotation
+                double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+                double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+                // Denominator is the largest motor power (absolute value) or 1
+                // This ensures all the powers maintain the same ratio, but only when
+                // at least one is out of the range [-1, 1]
+                double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+                double frontLeftPower = (rotY + rotX + rx) / denominator * speed;
+                double backLeftPower = (rotY - rotX + rx) / denominator * speed;
+                double frontRightPower = (rotY - rotX - rx) / denominator * speed;
+                double backRightPower = (rotY + rotX - rx) / denominator * speed;
+
+                // Set motor powers
+                motorFrontLeft.setPower(frontLeftPower);
+                motorBackLeft.setPower(backLeftPower);
+                motorFrontRight.setPower(frontRightPower);
+                motorBackRight.setPower(backRightPower);
+
+                telemetry.addData("Mode:","Field-Centric");
+                telemetry.addData("Speed", speed);
+                telemetry.addData("Front Left Power", frontLeftPower);
+                telemetry.addData("Back Left Power", backLeftPower);
+                telemetry.addData("Front Right Power", frontRightPower);
+                telemetry.addData("Back Right Power", backRightPower);
             } else {
-                runRobotCentricMode();
+                // Robot-Centric Mode
+                // Convert the raw x and y values to robot-centric forward and sideways velocities
+                forward = y;
+                sideways = x;
+                rotation = rx;
+
+                // Use the LT value as an acceleration factor.
+                // LT value is between 0 (not pressed) and 1 (fully pressed).
+                double lt = gamepad1.left_trigger;
+                double speed = accelerationFactor + (1 - accelerationFactor) * lt;
+
+                // Reset the yaw angle to 0 degrees when the "Back" button is pressed.
+                if (gamepad1.back) {
+                    imu.resetYaw();
+                }
+
+                // Calculate motor powers using mecanum drive kinematics
+                double frontLeftPower = (forward + sideways + rotation) * speed;
+                double backLeftPower = (forward - sideways + rotation) * speed;
+                double frontRightPower = (forward - sideways - rotation) * speed;
+                double backRightPower = (forward + sideways - rotation) * speed;
+
+                // Set motor powers
+                motorFrontLeft.setPower(frontLeftPower);
+                motorBackLeft.setPower(backLeftPower);
+                motorFrontRight.setPower(frontRightPower);
+                motorBackRight.setPower(backRightPower);
+
+                telemetry.addData("Mode:","Robot-Centric");
+                telemetry.addData("Speed", speed);
+                telemetry.addData("Front Left Power", frontLeftPower);
+                telemetry.addData("Back Left Power", backLeftPower);
+                telemetry.addData("Front Right Power", frontRightPower);
+                telemetry.addData("Back Right Power", backRightPower);
             }
+
+            telemetry.update();
         }
     }
-
-    private void runFieldCentricMode() {
-        double rawY = -gamepad1.left_stick_y;
-        double rawX = gamepad1.left_stick_x * 1.1;
-        double rawRX = gamepad1.right_stick_x;
-
-        double forward = rawY * Math.cos(getBotHeading()) + rawX * Math.sin(getBotHeading());
-        double sideways = -rawY * Math.sin(getBotHeading()) + rawX * Math.cos(getBotHeading());
-        double rotation = rawRX;
-
-        double lt = gamepad1.left_trigger;
-        double speed = accelerationFactor + (1 - accelerationFactor) * lt;
-
-        if (gamepad1.back) {
-            imu.resetYaw();
-        }
-
-        double frontLeftPower = (forward + sideways + rotation) * speed;
-        double backLeftPower = (forward - sideways + rotation) * speed;
-        double frontRightPower = (forward - sideways - rotation) * speed;
-        double backRightPower = (forward + sideways - rotation) * speed;
-
-        motorFrontLeft.setPower(frontLeftPower);
-        motorBackLeft.setPower(backLeftPower);
-        motorFrontRight.setPower(frontRightPower);
-        motorBackRight.setPower(backRightPower);
-
-        telemetry.addData("Speed", speed);
-        telemetry.addData("Front Left Power", frontLeftPower);
-        telemetry.addData("Back Left Power", backLeftPower);
-        telemetry.addData("Front Right Power", frontRightPower);
-        telemetry.addData("Back Right Power", backRightPower);
-        telemetry.update();
-    }
-
-    private void runRobotCentricMode() {
-// Get raw values from the gamepad
-        double y = -gamepad1.left_stick_y; // Negative because the gamepad's y-axis is inverted
-        double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-        double rx = gamepad1.right_stick_x;
-
-        // Convert the raw x and y values to robot-centric forward and sideways velocities
-        double forward = y * Math.cos(getBotHeading()) + x * Math.sin(getBotHeading());
-        double sideways = -y * Math.sin(getBotHeading()) + x * Math.cos(getBotHeading());
-        double rotation = rx;
-
-        // Use the LT value as an acceleration factor.
-        // LT value is between 0 (not pressed) and 1 (fully pressed).
-        double lt = gamepad1.left_trigger;
-        double speed = accelerationFactor + (1 - accelerationFactor) * lt;
-
-        // Reset the yaw angle to 0 degrees when the "Back" button is pressed.
-        if (gamepad1.back) {
-            imu.resetYaw();
-        }
-
-        // Calculate motor powers using mecanum drive kinematics
-        double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
-
-        // Rotate the movement direction counter to the bot's rotation
-        double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
-        double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
-
-        // Denominator is the largest motor power (absolute value) or 1
-        // This ensures all the powers maintain the same ratio, but only when
-        // at least one is out of the range [-1, 1]
-        double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
-        double frontLeftPower = (rotY + rotX + rx) / denominator * speed;
-        double backLeftPower = (rotY - rotX + rx) / denominator * speed;
-        double frontRightPower = (rotY - rotX - rx) / denominator * speed;
-        double backRightPower = (rotY + rotX - rx) / denominator * speed;
-
-        // Set motor powers
-        motorFrontLeft.setPower(frontLeftPower);
-        motorBackLeft.setPower(backLeftPower);
-        motorFrontRight.setPower(frontRightPower);
-        motorBackRight.setPower(backRightPower);
-
-        telemetry.addData("Speed", speed);
-        telemetry.addData("Front Left Power", frontLeftPower);
-        telemetry.addData("Back Left Power", backLeftPower);
-        telemetry.addData("Front Right Power", frontRightPower);
-        telemetry.addData("Back Right Power", backRightPower);
-        telemetry.update();
-    }
-
 
     // Helper method to get the robot's heading (yaw) from the IMU
     private double getBotHeading() {
